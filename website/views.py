6f2b7f91bf807ff0f78ce2a337e7d6cc74ca8a9c
Abeda Zahid Chandrica <64368728+Chandrica241@users.noreply.github.com>
2023-11-20 16:47:48 +0600

    Add files via upload

--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from flask import render_template, request
 from flask import redirect
 from flask import url_for
 from . import db
-from .models import Rooms
+from .models import Rooms, Payment, Guest, Booking
 import random
 views = Blueprint('views', __name__, template_folder='templates/home', static_folder='assets')
 
@@ -13,6 +13,56 @@ rooms = None
 @views.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+
+@views.route('/book', methods=['GET', 'POST'])
+def book():
+    if request.method == 'POST':
+        print(request.form)
+        own_name = request.form.get('ownName')
+        own_phone_number = request.form.get('ownPhoneNumber')
+        own_email = request.form.get('ownEmail')
+        own_country = request.form.get('ownCountry')
+        own_contact_details = request.form.get('ownContactDetails')
+        room_id = request.form.get('roomId')
+
+        guests = []
+        guest_count = int(request.form.get('guestCount'))
+        for i in range(1, guest_count + 1):
+            guest_name = request.form.get(f'guestName{i}')
+            guest_id = request.form.get(f'guestID{i}')
+            guests.append(Guest(guest_name=guest_name, guest_id=guest_id))
+
+        payment_method = request.form.get('paymentMethod')
+        amount = request.form.get('amount')
+        voucher = request.form.get('voucher')
+
+        # Create a new booking
+        booking = Booking(
+            own_name=own_name,
+            own_phone_number=own_phone_number,
+            own_email=own_email,
+            own_country=own_country,
+            own_contact_details=own_contact_details,
+            room_id=room_id,
+            guests=guests
+        )
+
+        # Add payment details
+        payment = Payment(
+            payment_method=payment_method,
+            amount=amount,
+            voucher=voucher
+        )
+
+        booking.payments.append(payment)
+        db.session.add(booking)
+        db.session.commit()
+        db.session.add(payment)
+        db.session.commit()
+
+        return render_template('confirmation.html')
+    return render_template('book.html')
 
 
 @views.route('/search', methods=['GET', 'POST'])
@@ -57,13 +107,13 @@ def room(room_id):
 def populate():
     room_types = ["single", "double", "suite"]
     room_sizes = ["kids", "single-adult", "For Couple"]
-    num_rooms = 10
+    num_rooms = 20
     for _ in range(num_rooms):
         room = Rooms(
             room_name=f"Room {random.randint(1, 100)}",
             room_type=random.choice(room_types),
             room_size=random.choice(room_sizes),
-            room_price=random.randint(50, 500),
+            room_price=random.randint(5000, 20000),
             room_view_images=f"image_{random.randint(0,8)}.jpg"
         )
         db.session.add(room)
